25086a62c6d96c3bbfd75202e16a9f2368368b50
enbes <cd725014@ohio.edu>
2018-02-23 16:39:30 -0500

    make_human_move documented

--- game.cc
+++ game.cc
@@ -170,7 +170,11 @@ void game::make_computer_move( )
 	make_move(best_move);
 }
 
-/// Processes the user's move, rejecting invalid input. Takes no parameters.
+/** 
+*	Processes the user's move, rejecting invalid input.
+*	Takes no parameters.
+*	@return void
+*/
 void game::make_human_move( ) {
 	string move;
 
